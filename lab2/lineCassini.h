32d0582e6f19080eed68da39ea2f1cbfff0be106
Oleg <olegkrygin9@gmail.com>
2021-09-29 16:35:56 +0300

    create file .cpp and .h for my 2nd lab

---
+++ lab2/lineCassini.h
@@ -0,0 +1,22 @@
+//
+// Created by oleg on 29.09.2021.
+//
+
+#ifndef LAB2_LINECASSINI_H
+#define LAB2_LINECASSINI_H
+
+struct Point {
+    int x;
+    int y;
+};
+
+class LineCassini {
+    private:
+        struct Point F1;
+        struct Point F2;
+        double a;
+        double c;
+
+};
+
+#endif //LAB2_LINECASSINI_H

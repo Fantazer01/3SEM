ebcd6d8c3d2c14b5744d72418bc4a14370896b13
Oleg <olegkrygin9@gmail.com>
2021-10-22 11:50:44 +0300

    lab3c: remove test cout

--- lab3c/Stack.cpp
+++ lab3c/Stack.cpp
@@ -53,14 +53,12 @@ namespace lab3c {
     }
 
     Stack::Stack(const Stack &stack):SZ(stack.SZ), top(stack.top) {
-        std::cout << "copy\n";
         array = new Data[SZ];
         for (int i = 0; i < top; ++i)
             array[i] = stack.array[i];
     }
 
     Stack::Stack(Stack && stack) noexcept:SZ(stack.SZ), top(stack.top), array(stack.array) {
-        std::cout << "move\n";
         stack.array = nullptr;
 
     }

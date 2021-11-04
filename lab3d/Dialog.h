be21be6f5ba81ffc17fb7e2352d45ddfb68cc997
Oleg <olegkrygin9@gmail.com>
2021-11-04 21:04:28 +0300

    lab3d: create new lab and files

---
+++ lab3d/Dialog.h
@@ -0,0 +1,18 @@
+//
+// Created by oleg on 04.11.2021.
+//
+
+#ifndef LAB3D_DIALOG_H
+#define LAB3D_DIALOG_H
+
+//#include ""
+
+namespace lab3d {
+
+    void introduction();
+
+
+}
+
+
+#endif //LAB3D_DIALOG_H
---
+++ lab3d/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by oleg on 04.11.2021.
+//
+
+#include <iostream>
+#include "Dialog.h"
+
+using namespace lab3d;
+
+int main() {
+    introduction();
+
+    return 0;
+}
+

f4fc8b1f3ad62d7c6ccf81177fd5a5e221f6ca62
Oleg <olegkrygin9@gmail.com>
2021-11-26 04:48:20 +0300

    finished description class Destroyer

--- lab4/GroupOfShips/Destroyer/Destroyer.cpp
+++ lab4/GroupOfShips/Destroyer/Destroyer.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Destroyer.h"
+
+namespace lab4 {
+
+    std::ostream& Destroyer::print(std::ostream &output) const
+    {
+        int i = 1;
+        this->Ship::print(output);
+        output << "covered ship: ";
+        if (covered == nullptr)
+            output << "no info" << std::endl;
+        else
+            output << covered->getName() << std::endl;
+
+        return output;
+    }
+
+    std::ostream& operator <<(std::ostream &output, Destroyer &ship)
+    {
+        ship.print(output);
+        return output;
+    }
+
+}
--- lab4/GroupOfShips/Destroyer/Destroyer.h
+++ lab4/GroupOfShips/Destroyer/Destroyer.h
@@ -5,17 +5,27 @@
 #ifndef LAB4_DESTROYER_H
 #define LAB4_DESTROYER_H
 
+#include <utility>
+
 #include "Ship.h"
 
 namespace lab4 {
 
     class Destroyer : public Ship {
     private:
+        typedef std::vector<Weapon> vecWeapon;
+    private:
         Ship *covered;
     public:
-        Destroyer();
-        Destroyer(std::string name, Person captain, int speed, int vitality, int teamMembers, std::vector<Weapon> _armament, Ship* _covered = nullptr);
+        Destroyer() = default;
+        Destroyer(std::string _name, Person _captain, uint _speed, uint _vitality, uint _teamMembers, vecWeapon _armament, Ship* _covered)
+        : Ship(std::move(_name), std::move(_captain), _speed, _vitality, _teamMembers, std::move(_armament)), covered(_covered) {}
 
+        Ship * getCovered() const { return covered; }
+        void setCovered(Ship *_covered) { covered = _covered; }
+
+        std::ostream& print(std::ostream &output) const override;//вроде вывел, но это не точно
+        friend  std::ostream& operator <<(std::ostream &output, Destroyer &ship);
     };
 
 }

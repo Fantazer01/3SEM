1ec077d1e128127759aa3e86f37a489b8c8e37c2
Oleg <olegkrygin9@gmail.com>
2021-11-26 06:07:24 +0300

    work with TableOfShips

--- lab4/GroupOfShips/TableOfShips/TableOfShips.cpp
+++ lab4/GroupOfShips/TableOfShips/TableOfShips.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "TableOfShips.h"
+
+namespace lab4 {
+
+    std::ostream& TableOfShips::print(std::ostream &output) const
+    {
+        for (const auto &obj : cells) {
+            output << "callsign" << obj.first << std::endl
+                   << obj.second;
+        }
+        return output;
+    }
+
+    std::ostream& operator <<(std::ostream &output, const TableOfShips &table)
+    {
+        table.print(output);
+        return output;
+    }
+}
--- lab4/GroupOfShips/TableOfShips/TableOfShips.h
+++ lab4/GroupOfShips/TableOfShips/TableOfShips.h
@@ -5,18 +5,68 @@
 #ifndef LAB4_TABLEOFSHIPS_H
 #define LAB4_TABLEOFSHIPS_H
 
-#include "../Ship/Ship.h"
+#include "Ship.h"
+#include <map>
 
-class Cell {
-    Ship *point;
-    std::string callsign;
-};
+namespace lab4 {
 
-class TableOfShips {
-private:
-    std::vector <Cell> cells;
+    class TableOfShips {
+    public:
+        class iterator {
+        private:
+            std::map<std::string, Ship*>::iterator it;
+        public:
+            iterator() = default;
+            explicit iterator(const std::map<std::string, Ship*>::iterator &_it): it(_it) {}
 
-};
+            bool operator ==(const iterator &out_it) const { return this->it == out_it.it; }
+            bool operator !=(const iterator &out_it) const { return this->it != out_it.it; }
+            iterator& operator ++() { ++it; return (*this); }
+            iterator operator ++(int) { iterator itCopy(it); ++it; return itCopy; }
+            std::pair<const std::string, Ship*>& operator *() { return *it; }
+            std::pair<const std::string, Ship*>* operator ->() { return &(*it); }
+
+        };
+
+        class const_iterator {
+        private:
+            std::map<std::string, Ship*>::const_iterator it;
+        public:
+            const_iterator() = default;
+            explicit const_iterator(const std::map<std::string, Ship*>::const_iterator &_it): it(_it) {}
+
+            std::map<std::string, Ship*>::const_iterator getCIT() const { return it; }
+
+            bool operator ==(const const_iterator &out_it) const { return this->it == out_it.it; }
+            bool operator !=(const const_iterator &out_it) const { return this->it != out_it.it; }
+            const_iterator& operator ++() { ++it; return (*this); }
+            const_iterator operator ++(int) { const_iterator itCopy(it); ++it; return itCopy; }
+            const std::pair<std::string, Ship*>& operator *() const { return *it; }
+
+        };
+    private:
+        std::map <std::string, Ship*> cells;//first - callsign, second - pointer
+    public:
+        TableOfShips() = default;
+
+        uint getNumOfGroup() const { return cells.size(); }
+
+        const_iterator begin() const { return const_iterator(cells.cbegin()); }
+        const_iterator end() const { return const_iterator(cells.cend()); }
+
+        Ship* find(const std::string& first) const { return cells.find(first)->second; }
+        void insert(const std::string& first, Ship * second) { cells.insert(std::make_pair(first, second)); }
+        void erase(const const_iterator &out_it) { cells.erase(out_it.getCIT()); }
+
+        std::ostream& print(std::ostream &output) const;
+        friend std::ostream& operator <<(std::ostream &output, const TableOfShips &table);
+
+        friend iterator;
+        friend const_iterator;
+    };
+
+}
+
 
 
 #endif //LAB4_TABLEOFSHIPS_H

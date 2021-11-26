dec66503a4e188a3b67c84b390a5793cd4ae8893
Oleg <olegkrygin9@gmail.com>
2021-11-26 03:48:55 +0300

    create type Target

---
+++ lab4/GroupOfShips/Target.h
@@ -0,0 +1,17 @@
+//
+// Created by oleg on 26.11.2021.
+//
+
+#ifndef LAB4_TARGET_H
+#define LAB4_TARGET_H
+
+namespace lab4 {
+
+    enum Target {
+        earth,
+        air
+    };
+
+}
+
+#endif //LAB4_TARGET_H

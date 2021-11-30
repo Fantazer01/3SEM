f0723fd2d204871ae98f0dde8b8be4262b060e07
Oleg <olegkrygin9@gmail.com>
2021-11-30 18:28:35 +0300

    description class GroupOfShips

--- lab4/GroupOfShips/GroupOfShips/GroupOfShips.h
+++ lab4/GroupOfShips/GroupOfShips/GroupOfShips.h
@@ -5,10 +5,45 @@
 #ifndef LAB4_GROUPOFSHIPS_H
 #define LAB4_GROUPOFSHIPS_H
 
+#include <utility>
 
-class GroupOfShips {
+#include "TableOfShips.h"
 
-};
+namespace lab4 {
+    class GroupOfShips {
+    private:
+        TableOfShips table;
+        Person commander;
+        std::string from;
+        std::string to;
+        double distance;//Расстояние между пунктами
+    public:
+        GroupOfShips() = default;
+        GroupOfShips(TableOfShips _table, Person _commander, double _distance)
+        :table(std::move(_table)), commander(std::move(_commander)), distance(_distance) {}
+
+        Person getCommander() const { return commander; }
+        std::string getDeparturePoint() const { return from; }
+        std::string getPointOfArrival() const { return to; }
+        double getDistance() const { return distance; }
+
+        void setCommander(Person _commander) { commander = std::move(_commander); }
+        void setDeparturePoint(std::string _from) { from = std::move(_from); }
+        void setPointOfArrival(std::string _to) { to = std::move(_to); }
+        void setDistance(double _distance) { distance = _distance; }
+
+        TableOfShips::const_iterator begin() const { return table.begin(); }
+        TableOfShips::const_iterator end() const { return table.end(); }
+
+        Ship* find(const std::string& first) const { return table.find(first); }
+        void insert(const std::string& first, Ship * second) { table.insert(first, second); }
+        void erase(const TableOfShips::const_iterator &out_it) { table.erase(out_it); }
+
+        uint getNumShipOfType() const;
+
+    };
+}
+
 
 
 #endif //LAB4_GROUPOFSHIPS_H

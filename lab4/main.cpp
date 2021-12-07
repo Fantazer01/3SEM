cdc34f811b2c018db659504457a418d19a315160
Oleg <olegkrygin9@gmail.com>
2021-12-07 21:42:38 +0300

    some rewrite main, unimportant

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "Person.h"
+#include "Weapon.h"
 #include "Plane.h"
+#include "Ship.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    lab4::Person MrProper;
-    lab4::Plane plane;
+    using namespace lab4;
+
+    FullName name = { "Best", "Proper", "Dear"};
+    Person MrProper(std::string("Mr"), name, 35);
+    std::cout << MrProper << std::endl;
+
+    Weapon one("pushka", "pulki", Weapon::light, 5, 5, 5);
+    std::cout << one;
+    Plane plane;
+    Ship ship("Lucky", MrProper, 5, 10, 15);
+    ship.print(std::cout);
     return 0;
 }

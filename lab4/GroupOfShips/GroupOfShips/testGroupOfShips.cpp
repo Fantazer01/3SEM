e52712bb575efa5eb720f9a48173bd8c8bcd47f1
Oleg <olegkrygin9@gmail.com>
2021-12-10 05:06:24 +0300

    comment cod test for Group

--- lab4/GroupOfShips/GroupOfShips/testGroupOfShips.cpp
+++ lab4/GroupOfShips/GroupOfShips/testGroupOfShips.cpp
@@ -101,15 +101,18 @@ TEST(GroupOfShips, Methods_setters_)
 
     ASSERT_EQ(2, group.getNumOfGroup());
 
-    ASSERT_EQ(&ship_AIRCRF, (*group.find("lucky")).second);
-    ASSERT_EQ(&destroyer, (*group.find("bull")).second);
+    AircraftCarrier air_ship2 = static_cast<AircraftCarrier&>(*(*table.find("lucky")).second);
+    ASSERT_EQ(ship_AIRCRF, air_ship2);
+
+    Destroyer dest_ship2 = static_cast<Destroyer&>(*(*table.find("bull")).second);
+    ASSERT_EQ(destroyer, dest_ship2);
     ASSERT_EQ(group.end(), group.find("baby"));
 
     ASSERT_EQ(1, group.getNumShipOfType(typeid(AircraftCarrier)));
     ASSERT_EQ(1, group.getNumShipOfType(typeid(Destroyer)));
 }
 
-TEST(GroupOfShips, Modeling)
+TEST(GroupOfShips, movePlane)
 {
     //begin of initialization
     Person set_smbds[] = {
@@ -133,7 +136,48 @@ TEST(GroupOfShips, Modeling)
     };
 
     AircraftCarrier ship_AIRCRF("Big ship", set_smbds[1], 15, 100, 7, weapons_light, planes);
-    Destroyer destroyer("Big ship", set_smbds[2], 15, 100, 7, weapons_light_and_hard);
+    AircraftCarrier ship_AIRCRF2("Big ship", set_smbds[2], 15, 100, 7, weapons_light);
+
+    TableOfShips table;
+    table.insert("lucky", &ship_AIRCRF);
+    table.insert("bull", &ship_AIRCRF2);
+    //end of initialization
+    GroupOfShips group(table, set_smbds[0], "New Delly", "San-Francisco", 12345.8);
+
+    ASSERT_EQ(2, dynamic_cast<AircraftCarrier&>(*(*(group.find("lucky"))).second).getNumPlanes());
+    ASSERT_EQ(0, dynamic_cast<AircraftCarrier&>(*(*(group.find("bull"))).second).getNumPlanes());
+
+    group.movePlane(dynamic_cast<AircraftCarrier&>(*(*(group.find("lucky"))).second).beginForPlane(), group.find("lucky"), group.find("bull"));
+
+    ASSERT_EQ(1, dynamic_cast<AircraftCarrier&>(*(*(group.find("lucky"))).second).getNumPlanes());
+    ASSERT_EQ(1, dynamic_cast<AircraftCarrier&>(*(*(group.find("bull"))).second).getNumPlanes());
+}
+/*
+TEST(GroupOfShips, Modeling)
+{
+    //begin of initialization
+    Person set_smbds[] = {
+            Person("boss", { "Simpson", "Tom", "Ivanovich"}, 15),
+            Person("minion", { "Pines", "Jo", "Petrovich"}, 6),
+            Person("hunky", { "Trump", "Peter", "Mikhailovich"}, 11)
+    };
+
+    std::vector<Weapon> weapons_light = {
+            Weapon("aka", "bullet", Weapon::light, 5, 6, 7)
+    };
+
+    std::vector<Weapon> weapons_light_and_hard = {
+            Weapon("aka", "bullet", Weapon::light, 5, 6, 7),
+            Weapon("bazooka", "rocket", Weapon::heavy, 8, 9,10)
+    };
+
+    vecPlane planes = {
+            Plane(weapons_light, 15, 100, 20, Plane::fighter),
+            Plane(weapons_light_and_hard, 17, 100, 23, Plane::bomber)
+    };
+
+    AircraftCarrier ship_AIRCRF("Big ship", set_smbds[1], 15, 1000, 7, weapons_light, planes);
+    Destroyer destroyer("Big ship", set_smbds[2], 15, 1300, 7, weapons_light_and_hard);
 
     TableOfShips table;
     table.insert("lucky", &ship_AIRCRF);
@@ -147,8 +191,9 @@ TEST(GroupOfShips, Modeling)
             Plane(weapons_light_and_hard, 17, 100, 23, Plane::bomber)
     };
     //end of initialization: enemy
+    ASSERT_EQ(fight_over, group.modeling(planes_enemy));
 }
-
+*/
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);

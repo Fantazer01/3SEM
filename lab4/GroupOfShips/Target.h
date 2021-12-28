5ab8d62bf977abf56270f04faa7792afff0e6164
Oleg <olegkrygin9@gmail.com>
2021-12-28 16:24:53 +0300

    add description for enum in Target.h

--- lab4/GroupOfShips/Target.h
+++ lab4/GroupOfShips/Target.h
@@ -2,18 +2,20 @@
 // Created by oleg on 26.11.2021.
 //
 
+    /*!
+     * @file
+     * @details Target is air for planes; target is earth for ships and other...
+     */
+
 #ifndef LAB4_TARGET_H
 #define LAB4_TARGET_H
 
 namespace lab4 {
 
-    /*!
-     * @file
-     * @details Target is air for planes; target is earth for ships and other...
-     */
+    /// Target can be 2 type
     enum Target {
-        earth,
-        air
+        earth, ///< earth mean that target-object isn't in air and not under water
+        air ///< air mean that target-object is in air
     };
 
 }
